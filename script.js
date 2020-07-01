c6340eb07b23767e618fd2272e213b3dad0e6996
elpen84 <elplist@gmail.com>
2020-07-01 00:25:22 -0700

    fixed fetch request
    added fetch request
    fixed api key

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // unsplash API
 const count = 10;
 const apiKey = "8k9wqLKGJVLBdvsec_GboXHYNoe0aE28IShmJF-0X-I";
-const apiUrl =
-  "https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//   Get Photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+//on load function
+getPhotos();
