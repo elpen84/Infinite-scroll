03e9ff42903f40055a6cdd08e8a5abfffd48f9ab
elpen84 <elplist@gmail.com>
2020-06-30 23:02:44 -0700

    3rd commit
    add img
    added loader img
    added loader svg
    added api
    styled container
    styled img container
    added apikey

---
+++ script.js
@@ -0,0 +1,5 @@
+// unsplash API
+const count = 10;
+const apiKey = "8k9wqLKGJVLBdvsec_GboXHYNoe0aE28IShmJF-0X-I";
+const apiUrl =
+  "https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}";
