d3e6f13f1d27ac0b73630e23393d9a32610abab2
elpen84 <elplist@gmail.com>
2020-07-01 19:19:14 -0700

    optimize function
    -remove ex images
    -removed rep code

--- script.js
+++ script.js
@@ -3,19 +3,35 @@ const loader = document.getElementById("loader");
 
 let photosArray = [];
 
+//helper function to set attributes on dom elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements for Links and Photo, aad to the DOM
 function displayPhotos() {
   //run  function for each object in photosarray
   photosArray.forEach((photo) => {
     //create <a> to link to unsplash
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "_blank");
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
     // create <img> for photo
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
     // put <img> inside <a>, then both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
